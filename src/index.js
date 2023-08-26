be6edc5c5b0c72d3ee9c76d8873d805447e8e4d7
Anu <anuoluwa.apiti@gmail.com>
2023-08-26 15:55:10 +0100

    feat -ui setup add file with css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
+
 const app = express();
+const server = http.createServer(app); // create the HTTP server using the Express app created on the previous line
+const io = socketio(server);
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+app.use(express.static(publicDirectoryPath));
+
+io.on("connection", () => {
+  // listen for new connections to Socket.IO
+  console.log("A new player connected!");
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Server is up on port ${port}.`);
+  server.listen(port, () => {
+    console.log(`Server is up on port ${port}.`);
+  });
 });
